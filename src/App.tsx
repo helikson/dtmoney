f5d117a6add2b66da30743e6824dd50280abc826
Helikson Maurilio Borges Rufino <helikson.rufino@sankhya.com.br>
2021-11-28 01:06:21 -0300

    add files to project

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
+import { Header } from './components/Header'
+import { Dashboard } from './components/Dashboard'
+import { NewTransactionModal } from './components/NewTransactionModal'
+import { GlobalStyle } from './styles/global'
+import { TransactionsProvider } from './hooks/useTransactions'
+
+Modal.setAppElement('#root')
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+  
+  return (
+    <TransactionsProvider>
+      <GlobalStyle isOpenModal={isNewTransactionModalOpen} />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
+    </TransactionsProvider>
+  );
+}
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,36 @@
+import { Container } from './styles'
+import { useTransactions } from '../../hooks/useTransactions'
+
+export function TransactionTable() {
+  const { transactions } = useTransactions()
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {transactions.map(({ id, title, type, amount, category, createdAt}) => (
+            <tr key={id}>
+              <td>{title}</td>
+              <td className={type}>
+                {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(amount)}
+              </td>
+              <td>{category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(new Date(createdAt))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  )
+}
